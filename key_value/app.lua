8ccd5c8c38a9386a82197268596fee5145e0d484
Satbek <set.turg@mail.ru>
2019-02-20 22:19:05 +0300

    added main page

--- key_value/app.lua
+++ key_value/app.lua
@@ -18,7 +18,7 @@ local set_up_base = function ()
             {name = 'key', type = 'string'},
             {name = 'value', type = 'string'}
         })
-        box.space.key_value:create_index('primary', {parts = {'key'}, type = 'HASH'})
+        box.space.key_value:create_index('primary', {parts = {'key'}})
         box.schema.user.grant('guest', 'read,write,execute', 'universe',
             nil, {if_not_exists = true})
         end
@@ -32,10 +32,11 @@ local function start_server()
         port = 8080
     end
     local server = require('http.server').new(nil, port)
-    server:route({ path = '/kv/:id', method = 'GET'}, handlers.get)
-    server:route({ path = '/kv', method = 'POST'}, handlers.add)
-    server:route({ path = '/kv/:id', method = 'DELETE'}, handlers.delete)
-    server:route({ path = '/kv/:id', method = 'PUT'}, handlers.update)
+    server:route({ path = '/kv/:id', method = 'GET' }, handlers.get)
+    server:route({ path = '/kv', method = 'POST' }, handlers.add)
+    server:route({ path = '/kv/:id', method = 'DELETE' }, handlers.delete)
+    server:route({ path = '/kv/:id', method = 'PUT' }, handlers.update)
+    server:route({ path = '/', method = 'GET', file = 'main.html.lua' })
     server:start()
     log.info('Server started on port = %d', port)
 end
---
+++ templates/main.html.lua
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Key-Value Storage</title>
+    </head>
+    <body>
+        <h1>Key-Value</h1>
+    </body>
+</html>
